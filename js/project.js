947f4110abd0b260452535a87dcdb78bc5acf3b6
Aaron Bond <aaron.reid.bond@gmail.com>
2023-06-09 14:58:15 -0700

    nearly 100%

--- js/project.js
+++ js/project.js
@@ -1,10 +1,11 @@
 function generateMovieHTML(movie) {
-    return `
+
+    let htmlD = `
           <div id="movie-${movie.id}" class="carousel-item" data-bs-interval="3000">
-            <div class="card mx-auto my-5 bg-transparent" style="width: 18rem;">
-                
-                <div class="card-body border rounded border-white">
-                    <h5 class="card-title text-light">${movie.title}</h5>
+            <div class="card mx-auto my-1 bg-light bg-opacity-10" style="width: 18rem;">
+                <div id="poster"></div>
+                <div class="card-body border border-top-0 rounded-bottom border-white">
+                    <h5 class="card-title text-light movieTitle">${movie.title}</h5>
                     <p class="card-text text-light">Rating: ${movie.rating}</p>
                     <button type="button" id="btnE-${movie.id}" class="btn btn-light text-light bg-transparent" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="movEdt(this.id)">Edit Movie</button>
                     <button id="btnD-${movie.id}" class="btn btn-light text-light bg-transparent delete-movie" data-id="${movie.id}" onclick="movDel(this.id)">Delete Movie</button>
@@ -12,6 +13,7 @@ function generateMovieHTML(movie) {
             </div>
           </div>
       `;
+    return htmlD
 }
 
 const moviesURL = 'https://excessive-sulfuric-narwhal.glitch.me/movies';
@@ -35,6 +37,7 @@ function loadMovies() {
             setTimeout(() => {
                 $('#movies-container').html(moviesHTML);
                 $('#movie-1').addClass('active');
+                getPoster()
             }, "3000");
         }
     });
@@ -52,6 +55,7 @@ function loadMoviesFast() {
             });
             $('#movies-container').html(moviesHTML);
             $('#movie-1').addClass('active');
+            getPoster()
         }
     });
 }
@@ -126,5 +130,24 @@ function movDel(id) {
         }
     });
 }
+
+
+function getPoster(){
+    $('.movieTitle').each(function (){
+        let film = $(this).text()
+        console.log(film)
+
+        $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=" + poasterKey + "&query=" + film, function(json) {
+            console.log(json)
+            if (json.results[0].original_title === film){
+                $('#poster').html(`<img class="w-100 border border-light rounded-top" id="thePoster" src="http://image.tmdb.org/t/p/w500/${json.results[0].poster_path}" class="img-responsive" >`);
+            } else {
+                $('#poster').html(`<img class="w-100 border border-light rounded-top" id="thePoster" src="img/rick.jpg"  class="img-responsive" />`);
+            };
+        })
+    });
+}
+
+
 
 loadMovies();
